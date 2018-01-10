c04e208a2c12bea399cf9ab11c87bf85ec64f426
ASDA-Grocery <32156105+ASDA-Grocery@users.noreply.github.com>
2018-01-10 11:31:43 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const express = require('express')
+    , bodyParser = require('body-parser')
+    , app = express()
+    , request = require("request");
+
+var google = require('googleapis')
+  , plus = google.plus('v1')
+  , OAuth2 = google.auth.OAuth2
+  , oauth2Client = new OAuth2(
+  691098961139-vii844i3lpk864qhf8r7jpkab1tv3j8h.apps.googleusercontent.com,
+  bIYU4h5RL1r-02kqLD8eJr2m,
+  https://oauth-redirect.googleusercontent.com/r/natasha-deb33
+);
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+var scopes = [
+  'https://www.googleapis.com/auth/plus.me',
+  'https://www.googleapis.com/auth/calendar'
+];
+
+var url = oauth2Client.generateAuthUrl({
+  // 'online' (default) or 'offline' (gets refresh_token)
+  access_type: 'offline',
+
+  // If you only need one scope you can pass it as a string
+  scope: scopes,
+
+  // Optional property that passes state parameters to redirect URI
+  // state: 'foo'
+});
+
+app.get('/getAuthUrl', function(req, res){
+  console.log('This is url: ', url)
+});
+
+app.get('/getTokenUrl', function(req, res){
+
+});
+
+app.listen((process.env.PORT || 8000), function() {
+    console.log("Server up and listening");
+});
